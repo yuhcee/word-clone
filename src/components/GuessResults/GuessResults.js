bd1736e2e5c14e325659ef3f068b861ef23df1b6
yuhcee <egbouchenna001@gmail.com>
2023-07-04 01:21:27 +0100

    :sparkles: display letters in each cell

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessResults({ guesses }) {
+    console.log(guesses);
     return (
-        <div class="guess-results">
-            {guesses.map(({ guess, id }) => (
-                <p key={id} class="guess">
-                    {guess}
+        <div className="guess-results">
+            {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+                <p className="guess" key={num}>
+                    {range(5).map((index) => (
+                        <span className="cell" key={index}>
+                            {guesses.length > num && guesses[num].guess.length > index ? guesses[num].guess[index] : ''}
+                        </span>
+                    ))}
                 </p>
             ))}
         </div>
