a95e4584388d6468f1309f5867b3b94b6aa0cc72
yuhcee <egbouchenna001@gmail.com>
2023-07-05 01:57:52 +0100

    :zap: pass game answer as prop to validate guess

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -19,7 +19,7 @@ function Game() {
     };
     return (
         <>
-            <GuessResults guesses={guesses} />
+            <GuessResults guesses={guesses} answer={answer} />
             <GuessInput handleAddGuess={handleAddGuess} />
         </>
     );
