c63ee591b2660ec97795f5bb86c1a6035ea04545
yuhcee <egbouchenna001@gmail.com>
2023-07-05 23:52:03 +0100

    :sparkles: refactor to create Guess component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,6 +6,7 @@ import GuessInput from '../GuessInput/GuessInput';
 import GuessResults from '../GuessResults/GuessResults';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import { checkGuess } from '../../game-helpers';
+import Banner from '../Banner/Banner';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -14,8 +15,9 @@ console.info({ answer });
 
 function Game() {
     const [guesses, setGuesses] = React.useState([]);
-    const gameLost = guesses.length >= NUM_OF_GUESSES_ALLOWED;
     const gameWon = guesses.length > 0 && checkGuess(guesses.at(-1), answer).every((result) => result.status === 'correct');
+    const gameLost = !gameWon && guesses.length >= NUM_OF_GUESSES_ALLOWED;
+    console.log({ gameLost, gameWon });
 
     const handleAddGuess = (guess) => {
         const newGuesses = [...guesses, guess];
@@ -25,6 +27,7 @@ function Game() {
         <>
             <GuessResults guesses={guesses} answer={answer} gameWon={gameWon} gameLost={gameLost} />
             <GuessInput handleAddGuess={handleAddGuess} disabled={gameLost || gameWon} />
+            <Banner gameWon={gameWon} gameLost={gameLost} guessCount={guesses.length} answer={answer} />
         </>
     );
 }
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import { range } from '../../utils';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
-import { checkGuess } from '../../game-helpers';
-import Banner from '../Banner/Banner';
+import Guess from '../Guess/Guess';
 
-function GuessResults({ guesses, answer, gameWon, gameLost }) {
-    const checkGuessResults = guesses.map((guess) => checkGuess(guess, answer));
-
+function GuessResults({ guesses, answer }) {
     return (
         <div className="guess-results">
-            {range(NUM_OF_GUESSES_ALLOWED).map((num) => {
-                const currentGuess = guesses[num] ? guesses[num] : '';
-                const currentCheckGuessResult = checkGuessResults[num] || [];
-
-                return (
-                    <p className="guess" key={num}>
-                        {range(5).map((index) => {
-                            const currentLetter = currentGuess[index] || '';
-                            const cellStatus = currentGuess.length && currentCheckGuessResult.length ? currentCheckGuessResult[index].status : '';
-                            return (
-                                <span className={`cell ${cellStatus}`} key={index}>
-                                    {currentLetter}
-                                </span>
-                            );
-                        })}
-                    </p>
-                );
-            })}
-            <Banner gameWon={gameWon} gameLost={gameLost} guessCount={guesses.length} answer={answer} />
+            {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+                <Guess key={num} currentGuess={guesses[num]} answer={answer} />
+            ))}
         </div>
     );
 }
