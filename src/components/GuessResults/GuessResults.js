e077f553a57c453c7d788658f6a422d998f90694
yuhcee <egbouchenna001@gmail.com>
2023-07-05 01:59:41 +0100

    :sparkles: apply status to letters

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { range } from '../../utils';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { checkGuess } from '../../game-helpers';
 
-function GuessResults({ guesses }) {
-    console.log(guesses);
+function GuessResults({ guesses, answer }) {
     return (
         <div className="guess-results">
             {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
                 <p className="guess" key={num}>
-                    {range(5).map((index) => (
-                        <span className="cell" key={index}>
-                            {guesses.length > num && guesses[num].guess.length > index ? guesses[num].guess[index] : ''}
-                        </span>
-                    ))}
+                    {range(5).map((index) => {
+                        const validateGuess = guesses.length > num ? checkGuess(guesses[num].guess, answer) : [{}];
+
+                        return (
+                            <span className={`cell ${validateGuess[index] ? validateGuess[index].status : ''}`} key={index}>
+                                {guesses.length > num && guesses[num].guess.length > index ? guesses[num].guess[index] : ''}
+                            </span>
+                        );
+                    })}
                 </p>
             ))}
         </div>
