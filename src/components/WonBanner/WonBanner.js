733da32e9c19d0dfa77126440fcfd7a6b1dd2e9b
yuhcee <egbouchenna001@gmail.com>
2023-07-07 00:37:28 +0100

    :zap: update WonBanner to handle restart

--- src/components/WonBanner/WonBanner.js
+++ src/components/WonBanner/WonBanner.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 
-function WonBanner({ numOfGuesses }) {
+function WonBanner({ numOfGuesses, handleRestart }) {
     return (
-        <Banner status={'happy'}>
+        <Banner status={'happy'} action={handleRestart} actionText="Restart game">
             <p>
                 <strong>Congratulations!</strong> Got it in <strong>{numOfGuesses === 1 ? '1 guess' : `${numOfGuesses} guesses`}</strong>.
             </p>
