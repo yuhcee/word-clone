69f3a9bc5f99fc1e7aa54fdd59a36c012db0cabc
yuhcee <egbouchenna001@gmail.com>
2023-07-06 23:38:59 +0100

    :sparkles: refactor Banner to be more resuable

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,24 +1,7 @@
 import React from 'react';
 
-function Banner({ gameWon, gameLost, guessCount, answer }) {
-    return (
-        <>
-            {gameWon && (
-                <div className="happy banner">
-                    <p>
-                        <strong>Congratulations!</strong> Got it in <strong>{guessCount} guesses</strong>.
-                    </p>
-                </div>
-            )}
-            {gameLost && (
-                <div className="sad banner">
-                    <p>
-                        Sorry, the correct answer is <strong>{answer}</strong>.
-                    </p>
-                </div>
-            )}
-        </>
-    );
+function Banner({ status, children }) {
+    return <div className={`${status} banner`}>{children}</div>;
 }
 
 export default Banner;
